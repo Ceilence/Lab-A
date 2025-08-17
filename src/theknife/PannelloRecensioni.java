d8d12832de7039a432f6f88ffa97607ad36a1ded
davim <davim@Saro>
2025-08-17 18:57:48 +0200

    Merge origin/master
    # Conflicts:
    #	src/theknife/ArchivioRecensioni.java
    #	src/theknife/PannelloRis.java
    #	src/theknife/RisList.java

---
+++ src/theknife/PannelloRecensioni.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package theknife;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+/**
+ *
+ * @author SSSSUGOI
+ */
+public final class PannelloRecensioni extends JPanel{
+    private Ristorante ristorante;
+    private final int ALTEZZA_PANNELLO = 100;
+    // Icone (come prima)
+    
+    
+    
+    public PannelloRecensioni(RisList risList, GestoreArchivi gestore, Ristorante r, JPanel dettaglioPanel,JLabel dettaglioNome,      
+    JLabel dettaglioCucina,
+    JLabel dettaglioImmagine,
+    JLabel labelDescrizione,
+    JButton detPref,
+    ImageIcon iconaBandiera // <-- aggiunto parametro
+) {
+    this.ristorante = r;
+
+    // Layout verticale con margini
+    setLayout(new BorderLayout());
+    setBorder(BorderFactory.createLineBorder(Color.GRAY));
+    setBackground(Color.WHITE);
+    setMaximumSize(new Dimension(Integer.MAX_VALUE, ALTEZZA_PANNELLO));
+    setPreferredSize(new Dimension(0, ALTEZZA_PANNELLO));
+    setMinimumSize(new Dimension(0, ALTEZZA_PANNELLO));
+
+    // --- SEZIONE SCRITTE ---
+    JPanel panelScritte = new JPanel();
+    panelScritte.setLayout(new BoxLayout(panelScritte, BoxLayout.Y_AXIS));
+    panelScritte.setOpaque(false);
+
+    JLabel titolo = new JLabel(ristorante.getNomeRis());
+    titolo.setFont(new Font("Arial", Font.BOLD, 16));
+
+    JLabel cuis = new JLabel(ristorante.getCuisRis());
+    cuis.setFont(new Font("Arial", Font.PLAIN, 14));
+
+    panelScritte.add(titolo);
+    panelScritte.add(cuis);
+
+    add(panelScritte, BorderLayout.NORTH);
+
+    // --- IMMAGINE ---
+    JLabel immagine = new JLabel(iconaBandiera); // <-- uso immagine passata
+    immagine.setPreferredSize(new Dimension(60, 40));
+    immagine.setOpaque(true);
+    immagine.setBackground(Color.WHITE);
+    immagine.setHorizontalAlignment(SwingConstants.CENTER);
+    add(immagine, BorderLayout.WEST);
+
+    // --- LISTENER PER CLICK ---
+    addMouseListener(new MouseAdapter() {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            gestore.getArchivioRis().setRisAttuale(ristorante);
+            dettaglioNome.setText(ristorante.getNomeRis());
+            dettaglioCucina.setText(ristorante.getCuisRis());
+            labelDescrizione.setText("<html><p style='width:635px'>" + ristorante.getDesRis() + "</p></html>");
+            dettaglioImmagine.setIcon(iconaBandiera); // <-- anche qui uso immagine già pronta
+
+            gestore.getArchivioPreferiti().setPrefAttuale(
+                ristorante.getIdRis(),
+                gestore.getArchivioUtenti().getUtenteAttuale().getIdUtente()
+            );
+
+            risList.aggiornaDetPref();
+
+            dettaglioPanel.revalidate();
+            dettaglioPanel.repaint();
+        }
+    });
+}
+    public Ristorante getRistorante() {
+        return ristorante;
+    }
+
+}

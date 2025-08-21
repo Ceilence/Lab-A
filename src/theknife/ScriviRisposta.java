32142a58420da6e8c2f86a397460bd28cdcd26bf
anpar <anpar@Notebook_Gio>
2025-08-21 23:21:46 +0200

    Merge origin/master
    # Conflicts:
    #	data/Commenti.csv
    #	src/theknife/CommentiRistoranti.java

--- src/theknife/ScriviRisposta.java
+++ src/theknife/ScriviRisposta.java
@@ -251,6 +251,7 @@ public class ScriviRisposta extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(null, "Inserisci un testo valido");
         } else {
             gestore.getArchivioCommenti().aggiungiCommento(new CommentiRistoranti(gestore.getArchivioUtenti().getUtenteAttuale().getIdUtente(), areaTesto.getText().trim(), commento.getIdCommento()));
+            
             this.dispose();
         }
     }//GEN-LAST:event_confermaButtonActionPerformed
